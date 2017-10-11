db34a3bece4d7adb5ee4b534e6fac0f43780a2b9
Schittecatte <nielsschittecatte.7@gmail.com>
2017-10-11 20:56:01 +0200

    Eerste commit

---
+++ TuinAanlegger/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TuinAanlegger.MvcApplication" Language="C#" %>
---
+++ TuinAanlegger/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TuinAanlegger.Startup))]
+namespace TuinAanlegger
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
